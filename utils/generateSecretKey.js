382bbfce9bdbd0b6f68a84e27e382395e5176038
Sergey Fedorov <sergey14fedorov@gmail.com>
2023-12-27 01:22:45 +0300

    initial

--- utils/generateSecretKey.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto')
-
-const generateRandomKey = () => {
-  return crypto.randomBytes(32).toString('hex')
-}
-
-const secretKey = generateRandomKey()
-console.log('Секретный ключ:', secretKey)
