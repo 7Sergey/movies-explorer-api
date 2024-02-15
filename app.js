54c872b254de7d87b5fc68429f1e7a452040111b
Sergey Fedorov <sergey14fedorov@gmail.com>
2024-02-15 19:24:23 +0530

    cors

--- app.js
+++ app.js
@@ -28,8 +28,6 @@ const limiter = rateLimit({
   message: 'Превышен лимит запросов, пожалуйста, подождите некоторое время.',
 })
 
-// app.js
-
 const options = {
   origin: ['http://localhost:3000', 'fedorov.movies.nomoredomainswork.ru'],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
@@ -60,33 +58,33 @@ app.use(errors()) // обработчик ошибок Celebrate
 // Централизованный обработчик ошибок
 // игнорируем ошибку eslint о неиспользованном аргументе
 // eslint-disable-next-line no-unused-vars
-app.use((error, req, res, next) => {
-  // если у ошибки нет статуса, выставляем 500
-  const { statusCode = 500, message } = error
-  // проверка на ошибки
+// app.use((error, req, res, next) => {
+//   // если у ошибки нет статуса, выставляем 500
+//   const { statusCode = 500, message } = error
+//   // проверка на ошибки
 
-  if (error.code === MONGO_DUPLICATE_ERROR_CODE) {
-    return res
-      .status(CONFLICT_ERROR_CODE)
-      .send({ message: 'Такой пользователь уже существует' })
-  }
-  if (error.name === 'CastError') {
-    return res
-      .status(CLIENT_ERROR_CODE)
-      .send({ message: 'Ошибка валидации полей' })
-  }
+//   if (error.code === MONGO_DUPLICATE_ERROR_CODE) {
+//     return res
+//       .status(CONFLICT_ERROR_CODE)
+//       .send({ message: 'Такой пользователь уже существует' })
+//   }
+//   if (error.name === 'CastError') {
+//     return res
+//       .status(CLIENT_ERROR_CODE)
+//       .send({ message: 'Ошибка валидации полей' })
+//   }
 
-  if (error.name === 'ValidationError') {
-    return res
-      .status(CLIENT_ERROR_CODE)
-      .send({ message: 'Ошибка валидации полей' })
-  }
+//   if (error.name === 'ValidationError') {
+//     return res
+//       .status(CLIENT_ERROR_CODE)
+//       .send({ message: 'Ошибка валидации полей' })
+//   }
 
-  return res.status(statusCode).send({
-    // проверяем статус и выставляем сообщение в зависимости от него
-    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
-  })
-})
+//   return res.status(statusCode).send({
+//     // проверяем статус и выставляем сообщение в зависимости от него
+//     message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+//   })
+// })
 
 app.listen(3002, () => {
   console.log(`Сервер запущен на ${3002} порту`)
