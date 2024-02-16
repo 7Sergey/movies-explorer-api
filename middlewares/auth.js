4b603561bf3cc4a48d981d844b8e9c976b59a29e
Sergey Fedorov <sergey14fedorov@gmail.com>
2024-02-16 11:08:43 +0530

    cors

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,18 +5,23 @@ require('dotenv').config() // Подключаем переменные окру
 const { JWT_SECRET, NODE_ENV } = process.env
 
 function auth(req, res, next) {
+  console.log('JWT_SECRET:', JWT_SECRET)
+  console.log('NODE_ENV:', NODE_ENV)
   try {
     const authHeader = req.headers.authorization
+    console.log('authHeader:', authHeader)
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
       throw new UnauthorizedError('NotAuthenticate')
     }
 
     const token = authHeader.replace('Bearer ', '')
+    console.log('token:', token)
 
     const payload = jwt.verify(
       token,
       NODE_ENV !== 'production' ? JWT_SECRET : 'dev_secret',
     )
+    console.log('payload:', payload)
 
     req.user = payload
     next()
